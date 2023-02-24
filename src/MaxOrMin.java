0c131fed4c30ad6bf5702c96dd98d885d5e74edd
Louis Fores <114371230+LousFores@users.noreply.github.com>
2023-02-24 09:30:03 +0700

    Update MaxOrMin.java

--- src/MaxOrMin.java
+++ src/MaxOrMin.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class MaxOrMin {
+import java.util.Scanner;
+
+public class MaxOrMin {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] array = new int[10];
+        int choice;
+        System.out.println("Choice = 1, Start program.");
+        System.out.println("Choice = 0, Exit program.");
+        do {
+            System.out.print("Your choice: ");
+            choice = input.nextInt();
+            if (choice == 1) {
+                for (int i=0; i<array.length; i++) {
+                    array[i] = (int) (Math.random()*100);
+                }
+                System.out.print("Show array: ");
+                for (int i=0; i<array.length; i++) {
+                    System.out.print(array[i]+" ");
+                }
+                System.out.println();
+                System.out.println("Max Value = " + maxValue(array));
+                System.out.println("Min Value = " + minValue(array));
+            } else {
+                System.exit(0);
+            }
+
+        } while (choice != 0 );
+    }
+    public static int maxValue(int[] arr) {
+        int max=arr[0];
+        for (int i=0; i<arr.length; i++) {
+           if (arr[i]>max) max = arr[i];
+        }
+        return max;
+    }
+    public static int minValue(int[] arr) {
+        int min=arr[0];
+        for (int i=0; i<arr.length; i++) {
+            if (arr[i]<min) min = arr[i];
+        }
+        return min;
+    }
+
 }
